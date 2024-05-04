dc6592ccefca5855119257f2cbda7b8e5e4616fa
Karolina Jonczyk <253737@student.pwr.edu.pl>
2024-05-04 16:24:19 +0200

    services

--- wsb-jpa1/src/main/java/com/capgemini/wsb/service/DoctorService.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/service/DoctorService.java
@@ -1,7 +1,7 @@
 package com.capgemini.wsb.service;
 import com.capgemini.wsb.dto.DoctorTO;
 
-public interface DoctorServiceImpl
+public interface DoctorService
 {
     DoctorTO findById(final Long id);
 }
--- wsb-jpa1/src/main/java/com/capgemini/wsb/service/impl/DoctorServiceImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/service/impl/DoctorServiceImpl.java
@@ -1,19 +1,16 @@
 package com.capgemini.wsb.service.impl;
-import com.capgemini.wsb.persistence.dao.PatientDAO;
+import com.capgemini.wsb.persistence.dao.DoctorDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class PatientServiceImpl {
-    private final PatientDAO patientDAO;
+public class DoctorServiceImpl {
+    private final DoctorDao doctorDao;
 
     @Autowired
-    public PatientServiceImpl(PatientDAO pPatientDao) {
-        this.patientDAO = pPatientDao;
+    public DoctorServiceImpl(DoctorDao pDoctorDao) {
+        this.doctorDao = pDoctorDao;
     }
-
 }
-
-
